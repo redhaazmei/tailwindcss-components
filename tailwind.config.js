a555184e7f9c08f1f91ad183cf32a1f767be31b6
Redha Azmei <redha.azmei@gmail.com>
2020-05-21 22:21:46 +0800

    Update tailwind.config and index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      body: ["Inter", "sans-serif"],
+      display: ["Nunito", "sans-serif"],
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
